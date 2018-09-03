3161400d2dea3ef397e3612808c32ec682b0950f
root <158426567@qq.com>
2018-09-03 16:24:53 +0800

    第二次提交

---
+++ src/com/test.java
@@ -0,0 +1,12 @@
+package com;
+
+public class test {
+
+    public static void main(String[] args) {
+        System.out.println("这是git的测试");
+        System.out.println("这是git的测试");
+        System.out.println("这是git的测试");
+        System.out.println("这是git的测试");
+        System.out.println("这是git的测试");
+    }
+}
